48348c81955e9215829f894835412cbb6871a9a8
Dev Shubh <shubhidave007@gmail.com>
2025-01-26 14:10:33 +0000

    v0.3

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,10 +1,15 @@
-import { initializeApp, getApps, cert } from "firebase-admin/app"
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+// Parse service account key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
+// Initialize Firebase Admin app only if it's not already initialized
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
-  })
+  });
 }
 
+// Export Firebase Admin Auth instance
+export const adminAuth = getAuth();
